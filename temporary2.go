d84e104dbc13eaf1ddfdc42737b293f09550ccae
bojan <bojanradovski@gmail.com>
2024-07-21 23:54:43 +0200

    tag v1.0.0

--- temporary2.go
+++ temporary2.go
@@ -1,7 +1,14 @@
 package temporary2
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 func WhenListen(s string) string {
 	return "When he listened he heard: " + strings.ToUpper(s)
+}
+
+func FromV100() {
+	fmt.Println("This is text from temp2's version 1.0.0")
 }
